f33ef44329fd3c60b2e643cfce4a72df70cbc859
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-04-13 22:12:40 +0200

    script

--- scrip.js
+++ scrip.js
@@ -4,3 +4,4 @@ primerNumero = prompt("introduzca un numero");
 let segundoNumero;
 segundoNumero= prompt('introduzca un numero')
 vsuma = parseInt(primerNumero)+parseInt(segundoNumero);
+alert('La suma es '+vsuma)
