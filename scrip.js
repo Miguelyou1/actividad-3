f0c3bdbf5e2f90c63f8bfbe136a3840de3ef5ea7
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-04-13 22:11:38 +0200

    script

--- scrip.js
+++ scrip.js
@@ -1,3 +1,6 @@
 let primerNumero;
 alert('Hola, a continuación se le pedirá un número para realizar unas operaciones ');
 primerNumero = prompt("introduzca un numero");
+let segundoNumero;
+segundoNumero= prompt('introduzca un numero')
+vsuma = parseInt(primerNumero)+parseInt(segundoNumero);
